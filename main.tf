23b4e6354164f21eeba22048a266fe6b6eb2deb8
kennedyuc <chukwukakennedyuche@gmail.com>
2022-10-21 05:26:10 -0700

    yaml files added

--- main.tf
+++ main.tf
@@ -2,21 +2,21 @@
 resource "kubectl_manifest" "issuer_manifest" {
     yaml_body = <<YAML
     apiVersion: cert-manager.io/v1
-kind: ClusterIssuer
-metadata:
-  name: letsencrypt-cluster
-  namespace: istio-system
-spec:
-  acme:
-    email: kennedy@mavencode.com
-    server: https://acme-v02.api.letsencrypt.org/directory
-    privateKeySecretRef:
-      name: letsencrypt-prod-cluster
-    solvers:
-    - http01:
-        ingress:
-          class: istio
-YAML
+    kind: ClusterIssuer
+    metadata:
+      name: letsencrypt-cluster
+      namespace: istio-system
+    spec:
+      acme:
+        email: kennedy@mavencode.com
+        server: https://acme-v02.api.letsencrypt.org/directory
+        privateKeySecretRef:
+          name: letsencrypt-prod-cluster
+        solvers:
+        - http01:
+            ingress:
+              class: istio
+    YAML
 }
 
 // create the certificate issuer resource
@@ -24,29 +24,29 @@ resource "kubectl_manifest" "cert_manifest" {
     depends_on  = [kubectl_manifest.issuer_manifest]
     yaml_body = <<YAML
     apiVersion: cert-manager.io/v1
-kind: Certificate
-metadata:
-  name: kenn-domain-cert
-  namespace: istio-system
-spec:
-  secretName: kenn-domain-cert
-  duration: 2160h
-  renewBefore: 360h
-  isCA: false
-  privateKey:
-    algorithm: RSA
-    encoding: PKCS1
-    size: 2048
-  usages:
-    - server auth
-    - client auth
-  dnsNames:
-    - "alpinresorts.online"
-  issuerRef:
-    name: letsencrypt-cluster
-    kind: ClusterIssuer
-    group: cert-manager.io
-YAML
+    kind: Certificate
+    metadata:
+      name: kenn-domain-cert
+      namespace: istio-system
+    spec:
+      secretName: kenn-domain-cert
+      duration: 2160h
+      renewBefore: 360h
+      isCA: false
+      privateKey:
+        algorithm: RSA
+        encoding: PKCS1
+        size: 2048
+      usages:
+        - server auth
+        - client auth
+      dnsNames:
+        - "alpinresorts.online"
+      issuerRef:
+        name: letsencrypt-cluster
+        kind: ClusterIssuer
+        group: cert-manager.io
+    YAML
 }
 
 // create the istio gateway resource
@@ -54,30 +54,30 @@ resource "kubectl_manifest" "gateway_manifest" {
     depends_on  = [kubectl_manifest.cert_manifest]
     yaml_body = <<YAML
     apiVersion: networking.istio.io/v1alpha3
-kind: Gateway
-metadata:
-  name: gateway
-  namespace: istio-system
-spec:
-  selector:
-    istio: ingressgateway
-  servers:
-  - port:
-      number: 80
-      name: http
-      protocol: HTTP
-    hosts:
-    - "alpinresorts.online"
-  - port:
-      number: 443
-      name: https
-      protocol: HTTPS
-    tls:
-      mode: SIMPLE
-      credentialName: kenn-domain-cert
-    hosts:
-    - "alpinresorts.online"
-YAML
+    kind: Gateway
+    metadata:
+      name: gateway
+      namespace: istio-system
+    spec:
+      selector:
+        istio: ingressgateway
+      servers:
+      - port:
+          number: 80
+          name: http
+          protocol: HTTP
+        hosts:
+        - "alpinresorts.online"
+      - port:
+          number: 443
+          name: https
+          protocol: HTTPS
+        tls:
+          mode: SIMPLE
+          credentialName: kenn-domain-cert
+        hosts:
+        - "alpinresorts.online"
+    YAML
 }
 
 // create the virtualservice resource
@@ -85,23 +85,23 @@ resource "kubectl_manifest" "virtualservice_manifest" {
     depends_on  = [kubectl_manifest.gateway_manifest]
     yaml_body = <<YAML
     apiVersion: networking.istio.io/v1alpha3
-kind: VirtualService
-metadata:
-  name: virtual-service-test
-  namespace: default
-spec:
-  hosts:
-  - "alpinresorts.online"
-  gateways:
-  - istio-system/gateway
-  http:
-  - match:
-    - uri:
-        exact: /
-    route:
-    - destination:
-        host: test-app-service
-        port:
-          number: 8080
-YAML
+    kind: VirtualService
+    metadata:
+      name: virtual-service-test
+      namespace: default
+    spec:
+      hosts:
+      - "alpinresorts.online"
+      gateways:
+      - istio-system/gateway
+      http:
+      - match:
+        - uri:
+            exact: /
+        route:
+        - destination:
+            host: test-app-service
+            port:
+              number: 8080
+    YAML
 }
